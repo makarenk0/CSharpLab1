83db4b48b71a762ce55e8fd195bcf44cf5cf2bf6
makarenk0 <45842815+makarenk0@users.noreply.github.com>
2020-02-21 21:05:05 +0200

    Добавьте файлы проекта.

---
+++ CSharpLab1/View/BirthDateControl.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CSharpLab1.View
+{
+    /// <summary>
+    /// Логика взаимодействия для BirthDateControl.xaml
+    /// </summary>
+    public partial class BirthDateControl : UserControl
+    {
+        public BirthDateControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
